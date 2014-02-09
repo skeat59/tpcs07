4676e0f160a7dd51fb7bc799f5aac095df55225a
skeat59 <gdalia_t@epita.fr>
2014-02-09 09:08:19 +0100

    suite

---
+++ tpcs7 sources/MyFarm/myFarm/myFarm/Farm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myFarm
+{
+    class Farm : Drawable
+    {
+        int size_x;
+        int size_y;
+        int[,] grounds = new int[10, 10];
+        static void array(int[,] tab)
+        {
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
